932bb0f2d68bd83c0ac4f90927354cc9a9354d48
jauntywunderkind <jaunty+wunder+kind+git@eldergods.com>
2019-03-16 23:59:26 -0400

    Switch to using async iterfaces via Continua, allowing hotplugged interfaces.

--- interfaces.js
+++ interfaces.js
@@ -1,9 +1,14 @@
+import Continua from "continua"
 import { networkInterfaces } from "os"
+
+export async function *interfaces( getInterfaces= interfacesProducer, options){
+	return yield *Continua( getInterfaces, options)
+}
 
 /**
 * Emit every address found on the system
 */
-export function * interfaces( interfaces= networkInterfaces()){
+export function * interfacesProducer( interfaces= networkInterfaces()){
 	for( const name of Object.keys( interfaces)){
 		const iface= interfaces[ name]
 		for( const addr of iface){
@@ -19,9 +24,21 @@ const _interfaces= interfaces
 /**
 * filter a set of addresses to only ipv4 matches
 */
-export function * ipv4( addresses= _interfaces()){
-	for( const addr of addresses){
+export async function * ipv4( addresses= _interfaces()){
+	for await( const addr of addresses){
 		if( addr.family!== "IPv4") {
+			continue
+		}
+		yield addr
+	}
+}
+
+/**
+* filter a set of addresses to only ipv6 matches
+*/
+export async function * ipv6( addresses= _interfaces()){
+	for await( const addr of addresses){
+		if( addr.family!== "IPv6") {
 			continue
 		}
 		yield addr
@@ -31,8 +48,8 @@ export function * ipv4( addresses= _interfaces()){
 /**
 * Pick off the address componenet from addresses
 */
-export function * addresses( interfaces= ipv4()){
-	for( const addr of interfaces){
+export async function * addresses( interfaces= ipv4()){
+	for await( const addr of interfaces){
 		yield addr.address
 	}
 }
